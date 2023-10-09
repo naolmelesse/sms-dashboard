6236864b3a14a77897530d56fc6e82ac592f61b5
Naol Melesse <naolmelesse91@gmail.com>
2023-10-09 19:53:20 +0300

    banner added for home

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,22 +1,22 @@
-
 import { FiSearch } from "react-icons/fi";
 import { FiMail } from "react-icons/fi";
 import { BiBell } from "react-icons/bi";
 import UserProfile from "./user-profile";
 import StudentQuickTabs from "./student-quick-tabs";
-
+import Link from "next/link";
 
 const Nav = () => {
 
     return(
-        <div className=" bg-slate-300">
-        <nav className="flex items-center py-2 px-20 justify-between">
-            <h2>SWIFT University</h2>
+        <div className="bg-[#232D3F] h-[60vh] select-none">
+        <nav className="flex items-start py-10 bg-[url('/university.jpg')] bg-cover px-20 text-[#fff] justify-between h-4/5">
+            <Link href="/" className="text-2xl">SWIFT University</Link>
 
-            <div className="flex items-center gap-10 text-slate-700">
-                {/* <div className="flex items-center gap-5">
-                    <FiSearch className="text-[1.5rem] text-slate-400"/>
-                    <input type="text" placeholder="Search..." className="py-1 outline-none bg-transparent" />
+
+            <div className="flex items-center gap-10">
+                {/* <div className="flex items-center gap-5 border border-[#fff]">
+                    <FiSearch className="text-[1.5rem]"/>
+                    <input type="text" placeholder="Search..." className="py-1 outline-none bg-transparent placeholder:text-[#fff]" />
                 </div> */}
             <FiMail className="text-2xl"/>
             <BiBell className="text-2xl"/>
--- app/components/student-quick-tabs.tsx
+++ app/components/student-quick-tabs.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 
 export default function StudentQuickTabs(){
     return(
-        <div className="w-3/5 mx-auto px-5 py-2 lg:px-10 flex text-[#fff] flex-wrap gap-5 lg:py-5">
-            <Link className="hover:bg-blue-300 p-2 transition duration-300" href="">Dashboard</Link>
-            <Link className="hover:bg-blue-300 p-2 transition duration-300" href="">Academic</Link>
-            <Link className="hover:bg-blue-300 p-2 transition duration-300" href="">Notices</Link>
-            <Link className="hover:bg-blue-300 p-2 transition duration-300" href="">Exam</Link>
-            <Link className="hover:bg-blue-300 p-2 transition duration-300" href="">Time Table</Link>
-            <Link className="hover:bg-blue-300 p-2 transition duration-300" href="">E-content</Link>
-            <Link className="hover:bg-blue-300 p-2 transition duration-300" href="">Fees</Link>
-            <Link className="bg-blue-300 p-2 transition duration-300" href="">More +</Link>
+        <div className=" mx-auto px-5 py-2 lg:px-20 flex text-[#fff] flex-wrap gap-5 lg:py-5">
+            <Link className="hover:bg-[#005B41] py-2 px-3 transition duration-300" href="">Dashboard</Link>
+            <Link className="hover:bg-[#005B41] py-2 px-3 transition duration-300" href="">Academic</Link>
+            <Link className="hover:bg-[#005B41] py-2 px-3 transition duration-300" href="">Notices</Link>
+            <Link className="hover:bg-[#005B41] py-2 px-3 transition duration-300" href="">Exam</Link>
+            <Link className="hover:bg-[#005B41] py-2 px-3 transition duration-300" href="">Time Table</Link>
+            <Link className="hover:bg-[#005B41] py-2 px-3 transition duration-300" href="">E-content</Link>
+            <Link className="hover:bg-[#005B41] py-2 px-3 transition duration-300" href="">Fees</Link>
+            <Link className="bg-[#005B41] p-2 transition duration-300" href="">More +</Link>
         </div>
     )
 }
